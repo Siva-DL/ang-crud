8ce8e07b0b9c4c8d33db47bc2e9fb3e9ddc77998
Siva-DL <sivaprasad.digitallync@gmail.com>
2022-10-26 20:27:21 +0530

    routing basic component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './routes/home/home.component';
+import { AboutComponent } from './routes/about/about.component';
+import { ExperienceComponent } from './routes/child/experience/experience.component';
+import { EducationComponent } from './routes/child/education/education.component';
+import { NotfoundComponent } from './routes/notfound/notfound.component';
+import { FooterComponent } from './common/footer/footer.component';
 
+const myRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'about',
+    component: AboutComponent,
+    children: [{ path: 'experience', component: ExperienceComponent }],
+  },
+  { path: '**', component: NotfoundComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    NotfoundComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(myRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
